51b027984cc73370ccabf13f01b83351445e0def
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-10-13 16:33:07 -0700

    printf

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+/* these below are the print func */
+int _putchar(char *c);
+int _puts(char *str);
+
+
+/* Declarations of .c files func*/
+int _printf(const char *format, ...);
+
+#endif
