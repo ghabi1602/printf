2933b4b662bb4b8f7bf82acbd5003ff6d17cc2d3
Abdessattar <ghabi.1602@gmail.com>
2023-10-14 06:01:53 -0700

    Add the print_int function

--- prints.c
+++ prints.c
@@ -29,10 +29,78 @@ void print_str(char *s)
 	}
 }
 /**
+ * reverse - reverses a string
+ * @str: array
+ * @length: size of the array
+ */
+void reverse(char str[], int length) 
+{
+    int start = 0;
+    int end = length - 1;
+
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+/**
+ * itoa - converts an int into a string
+ * @num: integer
+ * @str: string
+ * @base: integer
+ */
+
+char* _itoa(int num, char* str, int base) 
+{
+    int i = 0;
+    int isNegative = 0;
+
+    if (num == 0) {
+        str[i++] = '0';
+        str[i] = '\0';
+        return str;
+    }
+
+    if (num < 0 && base == 10) {
+        isNegative = 1;
+        num = -num;
+    }
+
+    while (num != 0) {
+        int rem = num % base;
+        str[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+        num = num / base;
+    }
+
+    if (isNegative)
+        str[i++] = '-';
+
+    str[i] = '\0';
+
+    reverse(str, i);
+
+    return str;
+}
+
+/**
  * print_int - prints an int
  * @n: integer
  */
+
 void print_int(int n)
 {
+	char *str;
+	int len;
 
+	_itoa(n, str, 10);
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	write(1, str, len);
 }
