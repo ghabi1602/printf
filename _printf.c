e680371521b43277adad94c01ac20b7fb6c79b3b
Ifeanyi Zion <ifeanyizion3@gmail.com>
2023-10-15 23:46:00 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -16,34 +16,32 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format && format[0] != '\0')
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
 	{
-		if (!format || (format[0] == '%' && !format[1]))
-			return (-1);
-		if (format[0] == '%' && format[1] == ' ' && !format[2])
-			return (-1);
-		for (p = format; *p; p++)
+		if (*p == '%')
 		{
+			p++;
 			if (*p == '%')
 			{
-				p++;
-				if (*p == '%')
-				{
-					length += _putchar('%');
-					continue;
-				}
-				p_func = print_data(*p);
+				length += _putchar('%');
+				continue;
+			}
+			p_func = print_data(*p);
 
-				length = (p_func)
-					? p_func(args)
-					: _printf("%%%c", *p);
-			}
-			else
-			{
-				length += _putchar(*p);
-			}
+			length += (p_func)
+				? p_func(args)
+				: _printf("%%%c", *p);
+		}
+		else
+		{
+			length += _putchar(*p);
 		}
 	}
+
 	va_end(args);
 	return (length);
 }
